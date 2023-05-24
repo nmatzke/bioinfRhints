0c36b5adca6dfe54521e4c4ab5af3d0fab2335f7
nmatzke <matzke@berkeley.edu>
2023-05-24 16:11:26 +1200

    playing

---
+++ flag/profiles_precalculated/ex_AQB_aln/mafft_constrained_aln_v1/589_seqs_mafftMiddleConstrained2_noHYPs2_muscle.fasta.cmd
@@ -0,0 +1,1 @@
+./gismo.mac /Users/nmat471/HD/GitHub/bioinfRhints/flag/profiles_precalculated/ex_AQB_aln/mafft_constrained_aln_v1/589_seqs_mafftMiddleConstrained2_noHYPs2_muscle.fasta -fast -rtf -seed=1684899005
